6c34de3ae98fd7fa9a3991f96089aeab302440ad
griddles <89864073+griddles@users.noreply.github.com>
2022-04-13 08:04:21 -0700

    made it better

--- home/index.php
+++ home/index.php
@@ -81,20 +81,20 @@
             }
             $i = 0;
             while ($i < count($sorteddatabase)){ // dynamic thumbnails
-                echo "
+                ?>
                 <div class='inline thumbnail'>
-                    <div class='thumbnailfade'>&nbsp</div>
-                    <a href='../video/?v=" . $sorteddatabase[$i][4] . "'><img src='../videos/thumbnails/" . $sorteddatabase[$i][4] . ".png' style='background-image:url(\"../images/thumbnailPlaceholder.png\");' width='320' height='180'></a>
+                    <div class='thumbnailfade'></div>
+                    <a href='../video/?v=<?php echo $sorteddatabase[$i][4]; ?>'><img src='../videos/thumbnails/<?php echo $sorteddatabase[$i][4]; ?>.png' style='background-image:url(\"../images/thumbnailPlaceholder.png\");' width='320' height='180'></a>
                     <div>
-                        <img class='creatoricon' src='../images/maskmid.png' style='background-image:url(\"../images/accountpfps/" . $sorteddatabase[$i][1] . ".png\")' width='32' height='32'>
+                        <img class='creatoricon' src='../images/maskmid.png' style='background-image:url(\"../images/accountpfps/<?php echo $sorteddatabase[$i][1]; ?>.png\")' width='32' height='32'>
                         <b>
-                            <div class='inline thumbnailtitle' title='" . $sorteddatabase[$i][0] . "' style='width:280px; text-overflow:ellipsis; overflow:auto;'>"
-                            . $sorteddatabase[$i][0] .
-                            "</div>
+                            <div class='inline thumbnailtitle' title='"<?php echo $sorteddatabase[$i][0]; ?>"' style='width:280px; text-overflow:ellipsis; overflow:auto;'>
+                                <?php echo $sorteddatabase[$i][0]; ?>
+                            </div>
                         </b>
                     </div>
                 </div>
-                ";
+                <?php
                 $i += 1;
             }
             CloseCon($conn);
