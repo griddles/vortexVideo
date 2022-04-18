c15c90e79c95c48bab3000abe139487a8adaa65a
griddles <89864073+griddles@users.noreply.github.com>
2022-04-18 09:06:16 -0700

    fixed masterpage, updating other pages

---
+++ masterpage/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device; initial=scale:1.0;">
+        <link rel="stylesheet" href="../reqs/globalStyle.css">
+        <link rel="icon" href="../images/vortexLogo.png">
+    </head>
+    <title>Vortex - []</title>
+    <body class="default">
+        <div class="sticky">
+            <a href="../home/" title="Vortex.com" style="margin-left:16px"><img src="../images/vortexFullLogo.png" width="240px"></a>
+            <form class="searchform" method="post" action="index.php">
+                <input class="searchbar" autocomplete="off" placeholder="Search" name="searchbar" id="searchbar">
+            </form>
+            <div class="accountbox">
+                <div class="account">
+                    <?php
+                    error_reporting(0);
+                    if ($_COOKIE["username"] == "")
+                    { ?>
+                    <a href="../login/"><button class="signin" id="signin">Sign In</button></a>
+                    <?php 
+                    }
+                    else
+                    { ?>
+                    <a href="../account/"><button class="signin" id="account">Account</button><a>
+                    <img class="pfp" id="pfp" src="../images/maskdark.png" style="background-image:url('../images/accountpfps/<?php echo $_COOKIE["username"]; ?>.png')" width="48px" height="48px">
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
+        <div class="sidebar">
+            <div class="navlink"><a href="../home/">Home</a></div>
+            <div class="navlink"><a href="../about/">About Us</a></div>
+        </div>
+    </body>
+</html>
