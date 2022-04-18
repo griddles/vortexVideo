bc75c753ca2301fef92603e4f78a4067258f47c4
griddles <89864073+griddles@users.noreply.github.com>
2022-04-18 09:08:25 -0700

    updated to new masterpage

--- uploadvideo/index.php
+++ uploadvideo/index.php
@@ -8,24 +8,28 @@
     <title>Vortex - Upload</title>
     <body class="default">
         <div class="sticky">
-            <a class="inline" href="../home/" title="Vortex.com" style="margin-left:16px"><img src="../images/vortexFullLogo.png" width="240px"></a>
-            <form class="inline" method="post" action="../home/">
+            <a href="../home/" title="Vortex.com" style="margin-left:16px"><img src="../images/vortexFullLogo.png" width="240px"></a>
+            <form class="searchform" method="post" action="index.php">
                 <input class="searchbar" autocomplete="off" placeholder="Search" name="searchbar" id="searchbar">
             </form>
-            <?php
-            error_reporting(0);
-            if ($_COOKIE["username"] == "")
-            { ?>
-            <a href="../login/"><button class="inline signin" id="signin">Sign In</button></a>
-            <?php 
-            }
-            else
-            { ?>
-            <a href="../account/"><button class="inline signin" id="account">Account</button><a>
-            <img class="inline pfp" id="pfp" src="../images/maskdark.png" style="background-image:url('../images/accountpfps/<?php echo $_COOKIE["username"]; ?>.png')" width="48px" height="48px">
-            <?php
-            }
-            ?>
+            <div class="accountbox">
+                <div class="account">
+                    <?php
+                    error_reporting(0);
+                    if ($_COOKIE["username"] == "")
+                    { ?>
+                    <a href="../login/"><button class="signin" id="signin">Sign In</button></a>
+                    <?php 
+                    }
+                    else
+                    { ?>
+                    <a href="../account/"><button class="signin" id="account">Account</button><a>
+                    <img class="pfp" id="pfp" src="../images/maskdark.png" style="background-image:url('../images/accountpfps/<?php echo $_COOKIE["username"]; ?>.png')" width="48px" height="48px">
+                    <?php
+                    }
+                    ?>
+                </div>
+            </div>
         </div>
         <div class="sidebar">
             <div class="navlink"><a href="../home/">Home</a></div>
