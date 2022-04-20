905ef44e2e73d1b182f48374fcac8443c3ad2ee9
griddles <89864073+griddles@users.noreply.github.com>
2022-04-20 10:02:43 -0700

    optimized

--- loggedin/index.php
+++ loggedin/index.php
@@ -30,34 +30,51 @@
             }
         }
     }
-    if ($usernamevalid != true) // this echo comment code stuff is unneccessary, see home page for a better way to do this
+    if ($usernamevalid != true) // error handling for invalid logins
     {
-        echo "
-        <html>
+        ?>
+        <html lang="en">
             <head>
-                <meta name='viewport' content='width=device; initial=scale:1.0;'>
-                <link rel='stylesheet' href='../reqs/globalStyle.css'>
-                <link rel='icon' href='../images/vortexLogo.png'>
+                <meta name="viewport" content="width=device; initial=scale:1.0;">
+                <link rel="stylesheet" href="../reqs/globalStyle.css">
+                <link rel="icon" href="../images/vortexLogo.png">
             </head>
-            <title>Vortex - Logged In</title>
-            <body class='default'>
-                <div class='sticky'>
-                    <a class='inline' href='../home/' title='Vortex.com' style='margin-left:16px'><img src='../images/vortexFullLogo.png' width='240px'></a>
-                    <form class='inline' method='post' action='index.php'>
-                        <input class='searchbar' autocomplete='off' placeholder='Search' name='searchbar' id='searchbar'>
+            <title>Vortex - About Us</title>
+            <body class="default">
+                <div class="sticky">
+                    <a href="../home/" title="Vortex.com" style="margin-left:16px"><img src="../images/vortexFullLogo.png" width="240px"></a>
+                    <form class="searchform" method="post" action="index.php">
+                        <input class="searchbar" autocomplete="off" placeholder="Search" name="searchbar" id="searchbar">
                     </form>
-                    <a href='../login/'><button class='inline signin' id='signin'>Sign In</button></a>
+                    <div class="accountbox">
+                        <div class="account">
+                            <?php
+                            error_reporting(0);
+                            if ($_COOKIE["username"] == "")
+                            { ?>
+                            <a href="../login/"><button class="signin" id="signin">Sign In</button></a>
+                            <?php 
+                            }
+                            else
+                            { ?>
+                            <a href="../account/"><button class="signin" id="account">Account</button><a>
+                            <img class="pfp" id="pfp" src="../images/maskdark.png" style="background-image:url('../images/accountpfps/<?php echo $_COOKIE["username"]; ?>.png')" width="48px" height="48px">
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
                 </div>
-                <div class='sidebar'>
-                    <div class='navlink'><a href='../home/'>Home</a></div>
-                    <div class='navlink'><a href='../about/'>About Us</a></div>
+                <div class="sidebar">
+                    <div class="navlink"><a href="../home/">Home</a></div>
+                    <div class="navlink"><a href="../about/">About Us</a></div>
                 </div>
-                <div class='body'>
+                <div class="body">
                     <h1>Invalid Login</h1>
                 </div>
             </body>
         </html>
-        ";
+        <?php
     }
     else
     {
@@ -67,7 +84,6 @@
         <meta name="viewport" content="width=device; initial=scale:1.0;">
         <link rel="stylesheet" href="../reqs/globalStyle.css">
         <link rel="icon" href="../images/vortexLogo.png">
-        <script src="../reqs/cookies.js"></script>
         <meta http-equiv="refresh" content="2; url='../account/'" />
     </head>
     <title>Vortex - Logged In</title>
