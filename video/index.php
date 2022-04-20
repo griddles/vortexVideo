d83d2efcebe91ed057e927f3dad8130a5533b254
griddles <89864073+griddles@users.noreply.github.com>
2022-04-20 09:59:08 -0700

    optimized

--- video/index.php
+++ video/index.php
@@ -30,17 +30,43 @@
             }
             $i += 1;
         }
-        if ($video_num == null) // error handling (need to update)
+        if ($video_num == null) // error handling
         {
-            echo "
-            <body class='default'>
-                <div class='body'>
-                    <h1>Video ID error</h1>
-                    <h3>" . $video_key . " is not a valid video ID</h3>
-                    <h4>The video you're looking for isn't here, it may have been removed or set to private.</h4>
+            ?>
+            <title>Vortex - Error</title>
+            <body class="default">
+                <div class="sticky">
+                    <a href="../home/" title="Vortex.com" style="margin-left:16px"><img src="../images/vortexFullLogo.png" width="240px"></a>
+                    <form class="searchform" method="post" action="index.php">
+                        <input class="searchbar" autocomplete="off" placeholder="Search" name="searchbar" id="searchbar">
+                    </form>
+                    <div class="accountbox">
+                        <div class="account">
+                            <?php
+                            error_reporting(0);
+                            if ($_COOKIE["username"] == "")
+                            { ?>
+                            <a href="../login/"><button class="signin" id="signin">Sign In</button></a>
+                            <?php 
+                            }
+                            else
+                            { ?>
+                            <a href="../account/"><button class="signin" id="account">Account</button><a>
+                            <img class="pfp" id="pfp" src="../images/maskdark.png" style="background-image:url('../images/accountpfps/<?php echo $_COOKIE["username"]; ?>.png')" width="48px" height="48px">
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
                 </div>
-            </body>
-            ";
+            <div class="sidebar">
+                <div class="navlink"><a href="../home/">Home</a></div>
+                <div class="navlink"><a href="../about/">About Us</a></div>
+            </div>
+            <div class="body">
+                <h3>This video ID is invalid.</h3>
+            </div>
+            <?php
         }
         else
         {
